155687932794978d97842c96cb1ada4da15b8da4
FreshBlood2019 <natalievolchkova@gmail.com>
2019-07-30 13:25:51 +0300

    Home task, lesson 4, car price, new car price.

---
+++ src/ProblemsHW/Problem01.java
@@ -0,0 +1,12 @@
+package ProblemsHW;
+
+public class Problem01 {
+
+    public static void main (String[] args) {
+        int x = 56;
+
+            System.out.println("Weight on the Moon = " + x*0.16);
+
+    }
+}
+
