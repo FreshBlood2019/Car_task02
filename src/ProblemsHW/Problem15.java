9663e2fb24de5705ad666f3afdb9418b896dfb3f
Natalie <52854052+FreshBlood2019@users.noreply.github.com>
2019-08-03 13:05:55 +0300

    Update Problem15.java

--- src/ProblemsHW/Problem15.java
+++ src/ProblemsHW/Problem15.java
@@ -2,19 +2,28 @@ package ProblemsHW;
 
 public class Problem15 {
     public static void main (String[] args) {
-        //int i;
-        //for (i=0; i<100; i++)
-        //System.out.println(i);
-        //int x = 0;
-        //while (x<100) {
-        //   x++;
-        //System.out.println(x);
-        int i = 0;
-        do {
-            i = i+1;
-            i++;
-        } while(i>100);
-        System.out.println(i);
+        int x;
+
+        for(x=0; x<=100; x++)
+            System.out.print(x + " ");
+        System.out.println();
     }
 }
+class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 100) {
+            System.out.print(i);
+            i++;
+        }
+    }
+}
+class DoWhile {
+    public static void main(String[] args) {
+        int n = 0;
 
+        do {
+            System.out.println(n);
+        } while (++n <= 100);
+    }
+}
